2a802a97d5d992e29be061e46dca74faa25239a7
saurabh9708 <guptashivam748@gmail.com>
2023-05-13 09:07:48 +0530

    SideScrollView

---
+++ lib/components/CategoriesWidgets.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class CategoriesWidget extends StatelessWidget {
+  const CategoriesWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          for (int i = 1; i < 5; i++)
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 8.0),
+              padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(20)),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "assets/cloths/$i.png",
+                    width: 60,
+                    height: 60,
+                  ),
+                  Text(
+                    "Black Tshirt dashing",
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                  ),
+                ],
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
+import 'components/CategoriesWidgets.dart';
 import 'widgets/HomeAppBar.dart';
 
 class MyHomeScreen extends StatelessWidget {
@@ -9,8 +12,72 @@ class MyHomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
-        children: const [
+        children: [
           HomeAppBar(),
+          SingleChildScrollView(
+            child: SafeArea(
+              child: Center(
+                child: Container(
+                  height: 500,
+                  padding: EdgeInsets.only(top: 15),
+                  decoration: BoxDecoration(
+                    color: Color(0xFFEDECF2),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(35),
+                      topRight: Radius.circular(35),
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 15),
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(left: 5),
+                              height: 50,
+                              width: 300,
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: "S e a r c h   h e r e . . . ."),
+                              ),
+                            ),
+                            Spacer(),
+                            Icon(
+                              Icons.camera_alt_outlined,
+                              size: 27,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.centerLeft,
+                        margin: EdgeInsets.symmetric(
+                          vertical: 20,
+                          horizontal: 10,
+                        ),
+                        child: Text(
+                          "Categories",
+                          style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black),
+                        ),
+                      ),
+                      CategoriesWidget(),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
